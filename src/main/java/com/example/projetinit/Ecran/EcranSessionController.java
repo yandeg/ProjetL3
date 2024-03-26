52e09c1266d7aab3051eb08ef14f3a73a2be9861
yandeg <yanisdegheb@gmail.com>
2024-03-26 18:25:06 +0100

    Session utilisateur

--- src/main/java/com/example/projetinit/Ecran/EcranSessionController.java
+++ src/main/java/com/example/projetinit/Ecran/EcranSessionController.java
@@ -1,16 +1,21 @@
 package com.example.projetinit.Ecran;
 
+import com.example.projetinit.donne.Session;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.List;
 
 public class EcranSessionController {
 
+    public TextField identificateur;
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -22,4 +27,41 @@ public class EcranSessionController {
         stage.setScene(scene);
         stage.show();
     }
+
+    private List<Session> sessions;
+
+    public void initialize() {
+        try {
+            sessions = Session.CSVReader.readSessionsFromCSV("sessions.csv");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void connexion(ActionEvent e) {
+        String identifiant = identificateur.getText();
+
+        boolean authentification = false;
+        for (Session session : sessions) {
+            if (session.getIdentifiant().equals(identifiant)) {
+                authentification = true;
+                break;
+            }
+
+        }
+        if (authentification) {
+            try {
+                switchToEcran0(e);
+            } catch (IOException ex) {
+                ex.printStackTrace();}
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Erreur");
+            alert.setHeaderText(null);
+            alert.setContentText("Un tel identifiant n'existe pas");
+            alert.showAndWait();
+        }
+
+    }
 }
+

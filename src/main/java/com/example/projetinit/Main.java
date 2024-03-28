827aabd1bce5dfa5dccda2628d06b8ca751482e4
RBOM12 <romainkiol@gmail.com>
2024-03-28 21:43:28 +0100

    fin.

--- src/main/java/com/example/projetinit/Main.java
+++ src/main/java/com/example/projetinit/Main.java
@@ -16,34 +16,7 @@ public class Main {
      * @param args Les arguments de la ligne de commande.
      */
     public static void main(String[] args) {
-
-
-        Affichage afficher = new Affichage();
-
-
-        List<Element> elements = getElements();
-        List<Chaines> productionChaine = getChaineProd();
-
-        // Affichage des éléments
-        System.out.println("Liste des éléments :");
-        for (Element element : elements) {
-            afficher.affichage(element.getCodeE() + ", " + element.getNomE() + ", " + element.getQuantite() + " " + element.getUnite());
-
-        }
-        afficher.affichage(ajouterStock("E008", 15));
-
-        System.out.println("Liste des éléments :");
-        for (Element element : elements) {
-            afficher.affichage(element.getCodeE() + ", " + element.getNomE() + ", " + element.getQuantite() + " " + element.getUnite());
-
-        }
-
-
-        // Affichage des chaînes de production
-        System.out.println("\nListe des chaînes de production :");
-        for (Chaines productionChain : productionChaine) {
-            System.out.println(productionChain.getCodeC() + ", " + productionChain.getNomC() + ", " + productionChain.getHashElementEntre()+", "+productionChain.getHashElementSortie());
-        }
+        MainApplication.main(args);
 
     }
 }
